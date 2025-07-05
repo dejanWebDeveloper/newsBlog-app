596dbe1c7c4cb8e504ecf2291982c3eeddedf7d0
Dejan <dejan_web@outlook.com>
2025-07-05 16:36:34 +0200

    aboutPageFrontEndComplete

---
+++ resources/views/front/about/partials/employee.blade.php
@@ -0,0 +1,46 @@
+<div class="section">
+    <div class="container">
+
+        <div class="row mb-5">
+            <div class="col-lg-5 mx-auto text-center" data-aos="fade-up">
+                <h2 class="heading text-primary">Our Team</h2>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="0">
+                <img src="images/person_1.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
+                <h5 class="text-black">James Griffin</h5>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+            </div>
+            <div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="100">
+                <img src="images/person_2.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
+                <h5 class="text-black">Claire Smith</h5>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+            </div>
+            <div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="200">
+                <img src="images/person_3.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
+                <h5 class="text-black">Jessica Wilson</h5>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+            </div>
+
+            <div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="0">
+                <img src="images/person_4.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
+                <h5 class="text-black">William Anderson</h5>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+            </div>
+            <div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="100">
+                <img src="images/person_5.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
+                <h5 class="text-black">Julie Harvey</h5>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+            </div>
+            <div class="col-lg-4 mb-4 text-center" data-aos="fade-up" data-aos-delay="200">
+                <img src="images/person_2.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-3">
+                <h5 class="text-black">Shana Clarkson</h5>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+            </div>
+        </div>
+
+    </div>
+</div>
