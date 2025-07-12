41c966dd0b27cda55522dc8f08d9f0762eef546e
Dejan <dejan_web@outlook.com>
2025-07-12 22:39:51 +0200

    latest_news_on_index_page_created

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,14 @@ class IndexController extends Controller
     public function index()
     {
         $categories = Category::all();
-        return view('front.index.index', compact('categories'));
+        $latestNews = Article::with('category')
+            ->where('ban', 0)
+            ->orderBy('created_at', 'desc')
+            ->limit(5)
+            ->get();
+        return view('front.index.index', compact(
+            'categories',
+            'latestNews'
+        ));
     }
 }
--- resources/views/front/index/partials/latest_news.blade.php
+++ resources/views/front/index/partials/latest_news.blade.php
@@ -1,58 +1,71 @@
+<!-- Start retroy layout blog posts -->
+
 <section class="section bg-light">
     <div class="container">
         <div class="row align-items-stretch retro-layout">
+            <!-- udjes na public/themes/index.html i tamo ces videti kakva je struktura bila ove sekcije
+            rad se sveo na sledece:
+                napravili smo foreach loop i u nju ubacili onaj deo koda koji se ponavlja u svih 5 divova, a to je <a></a>
+                zatim smo ono sto se razlikuje od prolaska do prolaska kroz kod definisali uz pomoc if i loop iteration
+                zatim smo pre samog koda koji je vidljiv na stranici a ispod foreach petlje definisali do koje iteracije se koeriste
+                podaci taj deo kooda, 1 i 2, 3, 4 i 5
+                *moglo je i bez continue i break
+            -->
             <div class="col-md-4">
-                <a href="#" class="h-entry mb-30 v-height gradient">
+                @foreach($latestNews as $leadArticle)
+                    @if($loop->iteration > 2)
+                        @break
+                    @endif
+                    <a href="{{route('single_page', ['heading'=>$leadArticle->heading])}}"
+                       class="h-entry @if($loop->iteration == 3) img-5 h-100 @endif @if($loop->iteration == 1 ||$loop->iteration == 4) mb-30 @endif @if($loop->iteration != 3) v-height @endif gradient">
 
-                    <div class="featured-img" style="background-image: url('images/img_2_horizontal.jpg');"></div>
-
-                    <div class="text">
-                        <span class="date">Apr. 14th, 2022</span>
-                        <h2>AI can now kill those annoying cookie pop-ups</h2>
-                    </div>
-                </a>
-                <a href="#" class="h-entry v-height gradient">
-
-                    <div class="featured-img" style="background-image: url('images/img_5_horizontal.jpg');"></div>
-
-                    <div class="text">
-                        <span class="date">Apr. 14th, 2022</span>
-                        <h2>Don’t assume your user data in the cloud is safe</h2>
-                    </div>
-                </a>
+                        <div class="featured-img" style="background-image: url('/themes/front/images/hero_2.jpg');"></div>
+                        <!-- ovo je jako bitno da ako smo uneli podatke u bazu i kada ih preko nekog query buildera vadimo iz baze ti dobijeni podaci su objekti klase u nasem slucaju DB i zato da bi ispisali neku
+                         njegovu vrednosti moramo da koristimo terminologiju za objkete a ne za nizove -> umesto ['']-->
+                        <div class="text">
+                            <span class="date">{{ $leadArticle->preheading }}</span>
+                            <h2>{{ $leadArticle->created_at->format('d M Y') }}</h2>
+                        </div>
+                    </a>
+                @endforeach
             </div>
             <div class="col-md-4">
-                <a href="#" class="h-entry img-5 h-100 gradient">
+                @foreach($latestNews as $leadArticle)
+                    @if($loop->iteration <= 2)
+                        @continue
+                    @endif
+                    @if(@$loop->iteration > 3)
+                        @break
+                    @endif
+                    <a href="{{route('single_page', ['heading'=>$leadArticle->heading])}}"
+                       class="h-entry @if($loop->iteration == 3) img-5 h-100 @endif @if($loop->iteration == 1 ||$loop->iteration == 4) mb-30 @endif gradient">
 
-                    <div class="featured-img" style="background-image: url('images/img_1_vertical.jpg');"></div>
+                        <div class="featured-img" style="background-image: url('/themes/front/images/hero_2.jpg');"></div>
 
-                    <div class="text">
-                        <span class="date">Apr. 14th, 2022</span>
-                        <h2>Why is my internet so slow?</h2>
-                    </div>
-                </a>
+                        <div class="text">
+                            <span class="date">{{ $leadArticle->preheading }}</span>
+                            <h2>{{ $leadArticle->created_at->format('d M Y') }}</h2>
+                        </div>
+                    </a>
+                @endforeach
             </div>
             <div class="col-md-4">
-                <a href="#" class="h-entry mb-30 v-height gradient">
+                @foreach($latestNews as $leadArticle)
+                    @if($loop->iteration < 4)
+                        @continue
+                    @endif
+                    <a href="{{route('single_page', ['heading'=>$leadArticle->heading])}}"
+                       class="h-entry  @if($loop->iteration == 1 || $loop->iteration == 4) mb-30 @endif @if($loop->iteration != 3) v-height @endif gradient">
 
-                    <div class="featured-img" style="background-image: url('images/img_3_horizontal.jpg');"></div>
+                        <div class="featured-img" style="background-image: url('/themes/front/images/hero_2.jpg');"></div>
 
-                    <div class="text">
-                        <span class="date">Apr. 14th, 2022</span>
-                        <h2>Startup vs corporate: What job suits you best?</h2>
-                    </div>
-                </a>
-                <a href="#" class="h-entry v-height gradient">
-
-                    <div class="featured-img" style="background-image: url('images/img_4_horizontal.jpg');"></div>
-
-                    <div class="text">
-                        <span class="date">Apr. 14th, 2022</span>
-                        <h2>Thought you loved Python? Wait until you meet Rust</h2>
-                    </div>
-                </a>
+                        <div class="text">
+                            <span class="date">{{ $leadArticle->preheading }}</span>
+                            <h2>{{ $leadArticle->created_at->format('d M Y') }}</h2>
+                        </div>
+                    </a>
+                @endforeach
             </div>
         </div>
     </div>
 </section>
-
